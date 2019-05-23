b5ac9f2849c520a7b8665d3d7b527f4c16937457
winChawakorn <chawakorn.su@ku.th>
2019-05-23 15:36:44 +0700

    Add firebase database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require('express')()
 const Twitter = require('twitter')
 const socketIO = require('socket.io')
+const firebase = require('./firebase')
+const moment = require('moment')
 
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -16,15 +18,44 @@ const app = server.listen(port, () => {
 })
 
 const io = socketIO.listen(app)
-io.on('connection', client => {
 
+const stream = client.stream('statuses/filter', { track: '#tradewar' })
+let events = []
+let newEvents = []
+firebase.database().ref('events').once('value', snapshots => {
+  if (snapshots.val())
+    events = [...(Object.values(snapshots.val()) || []), ...events]
+})
+
+io.on('connection', client => {
+  io.sockets.emit('new-message', events)
+  firebase.database().ref('data').once('value', snapshots => {
+    let data = []
+    if (snapshots.val())
+      data = Object.values(snapshots.val())
+    io.sockets.emit('new-data', data)
+  })
   client.on('disconnect', () => {
   })
 })
 
-const stream = client.stream('statuses/filter', { track: '#tradewar' })
 stream.on('data', function (event) {
   if (event) {
-    io.sockets.emit('new-message', event)
+    events.push({ text: event.text, createdAt: event.created_at })
+    io.sockets.emit('new-message', events)
+    newEvents.push({ text: event.text, createdAt: event.created_at })
+    firebase.database().ref('events').set(events)
   }
 })
+
+setInterval(() => {
+  firebase.database().ref('data').once('value', snapshots => {
+    let data = []
+    if (snapshots.val())
+      data = Object.values(snapshots.val())
+    data.push({ time: moment().format('HH:mm:ss DD-MM-YYYY'), count: newEvents.length })
+    io.sockets.emit('new-data', data)
+    firebase.database().ref('data').set(data)
+    newEvents = []
+  })
+}, 60000)
